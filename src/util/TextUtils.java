4e52e47abf1cfda3db40e7dd8ce60d843a657e87
jiajie <364844763@qq.com>
2015-10-23 09:01:22 +0800

    优化结构

--- src/util/TextUtils.java
+++ src/util/TextUtils.java
@@ -1,5 +1,7 @@
 package util;
 
+import java.util.ArrayList;
+
 /**
  *
  * 字符串处理工具类
@@ -20,4 +22,20 @@ public class TextUtils {
     public static boolean isEmpty(CharSequence str) {
         return str == null || str.length() == 0;
     }
+    //汉字返回拼音，字母原样返回，都转换为小写(默认取得的拼音全大写)
+    public static String getPinYin(String input) {
+        ArrayList<HanziToPinyin.Token> tokens = HanziToPinyin.getInstance().get(input);
+        StringBuilder sb = new StringBuilder();
+        if (tokens != null && tokens.size() > 0) {
+            for (HanziToPinyin.Token token : tokens) {
+                if (HanziToPinyin.Token.PINYIN == token.type) {
+                    sb.append(token.target);
+                } else {
+                    sb.append(token.source);
+                }
+            }
+        }
+        return sb.toString().toLowerCase();
+    }
+
 }
