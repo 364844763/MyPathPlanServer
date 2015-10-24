837450c528f2d9744a7a42829f6770ff162d565f
364844763 <364844763@qq.com>
2015-10-24 12:08:10 +0800

    路径修改

--- src/pathplan/PathFinding.java
+++ src/pathplan/PathFinding.java
@@ -4,9 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-/**
- * Created by Administrator on 2015/8/5.
- */
 public class PathFinding {
     private String start;
     private String ends;
